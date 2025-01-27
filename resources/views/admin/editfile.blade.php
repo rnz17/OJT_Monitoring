03396ee032964aaf5eb698955edd30ef9d2ab6a1
Josef Paurillo <josef26845@gmail.com>
2025-01-27 19:54:28 +0800

    wala na yung file type

--- resources/views/admin/editfile.blade.php
+++ resources/views/admin/editfile.blade.php
@@ -12,11 +12,9 @@
                 <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Column name:</label>
                 <input type="text" name="name" id="name" value="{{ $file->column_name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
             </div>
-            
+ 
             <div>
-                <label for="filetype" class="block text-gray-700 text-sm font-bold mb-2">File Type</label>
-                <input type="text" name="name" id="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-                <p class="text-gray-400">If you want to change file type, please delete the column and create new one to avoid conflicts with uploaded files</p>
+                <p class="text-gray-500 mb-4">If you want to change file type, please delete the column and create new one to avoid conflicts with uploaded files</p>
             </div>
             
             <div class="space-y-2">
